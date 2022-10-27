619f6077d48e6afd5571d03bf1d43202e246632b
Zaker <pavlenko_zk_hgs@mail.ru>
2022-10-27 22:11:35 +0300

    Day 1. Task 1-6

---
+++ src/main/java/day1/Task1.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class Task1 {
+    public static void main(String[] args) {
+    int a = 0;
+    while (a<10){
+        a+=1;
+        System.out.print("JAVA ");
+    }
+    }
+}
